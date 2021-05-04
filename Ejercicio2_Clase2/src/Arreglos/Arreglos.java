98e121839a13b493ae48e11a024a578d12abd6fd
JUAN OSPINA <juanhack@users.noreply.github.com>
2021-05-04 12:30:58 -0500

    Actualizacion del Segundo Punto clase 2

--- Ejercicio2_Clase2/src/Arreglos/Arreglos.java
+++ Ejercicio2_Clase2/src/Arreglos/Arreglos.java
@@ -20,6 +20,49 @@ public class Arreglos {
 
 	public static void main(String[] args) {
 		
+		/** Esta funcion resuelve el problema de las fechas
+		primerPunto();
+		*/
+		
+		/**Esta funcion resuelve el problema del arreglo de numeros*/
+		segundoPunto();
+		
+		
+		
+		
+
+	}
+	
+	private static void segundoPunto() {
+		/** Este codigo resuelve el tercer punto que es crear un arreglo de 5 enteros y sumar sus valores, encuentre el mayo y si la suma es 
+		 * mayor a 300 agregar el 10% y si no el 7%*/
+		Scanner entrada = new Scanner(System.in);
+		float suma = 0;
+		int mayor = 0;
+ 
+		int[] misEnteros = new int[5];
+		for (int i = 0; i < 5; i++) {
+			System.out.print("Ingrese el número en la posición " + (i) + " :");
+			misEnteros[i] = entrada.nextInt();
+		}
+		entrada.close();
+		// realizar la suma y encontrar el mayor
+				for (int i = 0; i < misEnteros.length; i++) {
+					suma += misEnteros[i];			
+					if (mayor < misEnteros[i]) {
+						mayor = misEnteros[i];
+					}
+					System.out.println(String.format("Posición [%d] Elemento: %d", i,
+							misEnteros[i]));
+				}
+		if(suma > 300) {
+			suma = (float) (suma * 1.10);
+		}else {
+			suma = (float) (suma * 1.07);
+		}
+	}
+	
+	private static void primerPunto() {
 		/** Este codigo resuelve el primer punto, se recibe por conola una cadena compuesta por “dd/MM/YYYY”, y separa los valores almacenando 
 		 * cada uno en una variable de dia, mes y año. */
 		
@@ -32,15 +75,6 @@ public class Arreglos {
 		/**Este codigo resuelve el segundo punto que es imprimir cada una de las variables*/
 		
 		System.out.println(dia + " " + mes + " " + anio);
-		
-		/** Este codigo resuelve el tercer punto que es crear un arreglo de 5 enteros y sumar sus valores*/
-		
-		int[] enteros = null;
-		
-		for(int i=0; i<5 ; i++) {
-			enteros[i]=i;
-		}
-		
 	}
 
 	/**Esta funcion tiene como proposito capturar informacion desde consola*/
@@ -49,6 +83,7 @@ public class Arreglos {
 		try (Scanner in = new Scanner(System.in)) {
 			System.out.println(textoPropuesto);
 			String fecha = in.nextLine();
+			in.close();
 			return fecha;
 		}
 	}
